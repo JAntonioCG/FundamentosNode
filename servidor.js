c1be2efc7b510b222575d6be484530f8acc23e85
JAntonioCG <juanantoniocastanongarcia@gmail.com>
2024-01-25 09:27:34 -0600

    instalamos firebase

--- servidor.js
+++ servidor.js
@@ -1,4 +1,5 @@
 const express = require('express')
+import { initializeApp } from "firebase/app";
 const app = express()
 const port = 6010
 
@@ -14,3 +15,21 @@ app.listen(port, () => {
     console.log('Servidor Escuchando: ', port)
     console.log(`Servidor Escuchando: ${port}`)
 })
+
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDemZ9PeOBLbcmsp1xCKpQV_Moxn3uUFjg",
+  authDomain: "crud-jacg.firebaseapp.com",
+  projectId: "crud-jacg",
+  storageBucket: "crud-jacg.appspot.com",
+  messagingSenderId: "365582993742",
+  appId: "1:365582993742:web:b193b26ebdab48b4fd761f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
